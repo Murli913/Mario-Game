f4cfbeabc78197c2cdb59402b158db0f0652fd51
Murli913 <murlitalreja913@gmail.com>
2024-05-13 10:52:52 +0530

    added the frontend part

---
+++ mario-frontend/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+import Gamee from './game';
+function App() {
+ 
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
